8f65fac4677e6d67a401a66d3423fe998d055cd4
Justaloneguy <51025635+Justaloneguy@users.noreply.github.com>
2021-09-10 19:55:49 +0530

    Added Buttons, Textbox and Buttons
    Also changed window size and heading font size.

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from tkinter import *
+from tkinter import ttk
 
 class Student:
     def __init__(self,root):
         self.root=root
         self.root.title("Student Management System")
-        self.root.geometry("1350x700+0+0")
+        self.root.geometry("1350x740+0+0")
 
-        title=Label(self.root, text= "Student Management System",font=("Arial",40,"bold"),bg="grey")
+        title=Label(self.root, text= "Student Management System",font=("Arial",35,"bold"),bg="Brown")
         title.pack(side=TOP,fill="x")
         
     #========Manage_Frame=======
         Manage_Frame = Frame(self.root,bd=4,relief=RIDGE,bg="pink")
-        Manage_Frame.place(x=20,y=100,width=450,height=560)
+        Manage_Frame.place(x=20,y=100,width=450,height=590)
 
         m_title=Label(Manage_Frame,text="Manage Students",font=("Arial",20,"bold"),bg="pink")
         m_title.grid(row=0,columnspan=2,pady=20)
@@ -36,7 +37,10 @@ class Student:
 
         lbl_gender= Label(Manage_Frame,text="Gender",font=("Arial",15,"bold"),bg="pink")
         lbl_gender.grid(row=4,column=0,pady=10,padx=20,sticky="w")
-        # add box
+        
+        combo_gender= ttk.Combobox(Manage_Frame,font=("Arial",15,"bold"),state="readonly")
+        combo_gender['values']=("male","female","other")
+        combo_gender.grid(row=4,column=1,padx=20,pady=10)
 
         lbl_contact= Label(Manage_Frame,text="Contact",font=("Arial",15,"bold"),bg="pink")
         lbl_contact.grid(row=5,column=0,pady=10,padx=20,sticky="w")
@@ -52,11 +56,22 @@ class Student:
 
         lbl_address= Label(Manage_Frame,text="Address",font=("Arial",15,"bold"),bg="pink")
         lbl_address.grid(row=7,column=0,pady=10,padx=20,sticky="w")
-        #add box
+        
+        txt_address = Text(Manage_Frame,width=31,height=4,font=("arial",10),bg="pink")
+        txt_address.grid(row=7,column=1,pady=10,padx=20,sticky="w")
 
-    #========Deatil_Frame========
+    #=======Button_Frame========
+        btn_Frame= Frame(Manage_Frame,bd=4,relief=RIDGE,bg="pink")
+        btn_Frame.place(x=10,y=500,width=420)
+
+        Addbtn= Button(btn_Frame,text="Add",width=10).grid(row=0,column=0,padx=10,pady=10)
+        Uddatebtn= Button(btn_Frame,text="Update",width=10).grid(row=0,column=1,padx=10,pady=10)
+        Deletebtn= Button(btn_Frame,text="Delete",width=10).grid(row=0,column=2,padx=10,pady=10)
+        Clearbtn= Button(btn_Frame,text="Clear",width=10).grid(row=0,column=3,padx=10,pady=10)
+
+    #========Deatil_Frame=======
         Detail_Frame = Frame(self.root,bd=4,relief=RIDGE,bg="pink")
-        Detail_Frame.place(x=500,y=100,width=800,height=560)
+        Detail_Frame.place(x=500,y=100,width=800,height=590)
 
 if __name__ == "__main__":
     root=Tk()
