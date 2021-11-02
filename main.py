8795977e2ebe77b9653c11215731f7b22c0e97ec
Debjeet Singha <51025635+Justaloneguy@users.noreply.github.com>
2021-11-02 12:20:51 +0530

    Update main.py
    changed colour

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pymysql
 import  tkinter.messagebox
 from PIL import ImageTk,Image
 
+
 db_name=""
 db_pass=""
 db_host=""
@@ -19,7 +20,7 @@ class Student:
         self.root.geometry("1350x740+200+150")
         self.root.resizable(False,False)
 
-        title=Label(self.root, text= "Student Management System",font=("Arial",35,"bold"),bg="pink")
+        title=Label(self.root, text= "Student Management System",font=("Arial",35,"bold"),bg="lightblue")
         title.pack(side=TOP,fill="x")
 
     #=======All_Variables=======
@@ -34,57 +35,57 @@ class Student:
         self.search_txt=StringVar()
         
     #========Manage_Frame=======
-        Manage_Frame = Frame(self.root,bd=4,relief=RIDGE,bg="pink")
+        Manage_Frame = Frame(self.root,bd=4,relief=RIDGE,bg="lightblue")
         Manage_Frame.place(x=20,y=100,width=450,height=590)
 
-        m_title=Label(Manage_Frame,text="Manage Students",font=("Arial",20,"bold"),bg="pink")
+        m_title=Label(Manage_Frame,text="Manage Students",font=("Arial",20,"bold"),bg="lightblue")
         m_title.grid(row=0,columnspan=2,pady=20)
 
-        lbl_roll=Label(Manage_Frame,text="Roll No.",font=("Arial",15,"bold"),bg="pink")
+        lbl_roll=Label(Manage_Frame,text="Roll No.",font=("Arial",15,"bold"),bg="lightblue")
         lbl_roll.grid(row=1,column=0,pady=10,padx=20,sticky="w")
 
-        txt_roll= Entry(Manage_Frame,textvariable=self.Roll_No_var,font=("Arial",15,"bold"),bg="pink")
+        txt_roll= Entry(Manage_Frame,textvariable=self.Roll_No_var,font=("Arial",15,"bold"),bg="lightblue")
         txt_roll.grid(row=1,column=1,pady=10,padx=20,sticky="w")
         
-        lbl_name= Label(Manage_Frame,text="Name",font=("Arial",15,"bold"),bg="pink")
+        lbl_name= Label(Manage_Frame,text="Name",font=("Arial",15,"bold"),bg="lightblue")
         lbl_name.grid(row=2,column=0,pady=10,padx=20,sticky="w")
 
-        txt_name= Entry(Manage_Frame,textvariable=self.name_var,font=("Arial",15,"bold"),bg="pink")
+        txt_name= Entry(Manage_Frame,textvariable=self.name_var,font=("Arial",15,"bold"),bg="lightblue")
         txt_name.grid(row=2,column=1,pady=10,padx=20,sticky="w")
 
-        lbl_email= Label(Manage_Frame,text="Email",font=("Arial",15,"bold"),bg="pink")
+        lbl_email= Label(Manage_Frame,text="Email",font=("Arial",15,"bold"),bg="lightblue")
         lbl_email.grid(row=3,column=0,pady=10,padx=20,sticky="w")
 
-        txt_email= Entry(Manage_Frame,textvariable=self.email_var,font=("Arial",15,"bold"),bg="pink")
+        txt_email= Entry(Manage_Frame,textvariable=self.email_var,font=("Arial",15,"bold"),bg="lightblue")
         txt_email.grid(row=3,column=1,pady=10,padx=20,sticky="w")
 
-        lbl_gender= Label(Manage_Frame,text="Gender",font=("Arial",15,"bold"),bg="pink")
+        lbl_gender= Label(Manage_Frame,text="Gender",font=("Arial",15,"bold"),bg="lightblue")
         lbl_gender.grid(row=4,column=0,pady=10,padx=20,sticky="w")
         
         combo_gender= ttk.Combobox(Manage_Frame,textvariable=self.gender_var,font=("Arial",15,"bold"),state="readonly")
         combo_gender['values']=("male","female","other")
         combo_gender.grid(row=4,column=1,padx=20,pady=10)
 
-        lbl_contact= Label(Manage_Frame,text="Contact",font=("Arial",15,"bold"),bg="pink")
+        lbl_contact= Label(Manage_Frame,text="Contact",font=("Arial",15,"bold"),bg="lightblue")
         lbl_contact.grid(row=5,column=0,pady=10,padx=20,sticky="w")
 
-        txt_contact= Entry(Manage_Frame,textvariable=self.contact_var,font=("Arial",15,"bold"),bg="pink")
+        txt_contact= Entry(Manage_Frame,textvariable=self.contact_var,font=("Arial",15,"bold"),bg="lightblue")
         txt_contact.grid(row=5,column=1,pady=10,padx=20,sticky="w")
 
-        lbl_dob= Label(Manage_Frame,text="D.O.B",font=("Arial",15,"bold"),bg="pink")
+        lbl_dob= Label(Manage_Frame,text="D.O.B",font=("Arial",15,"bold"),bg="lightblue")
         lbl_dob.grid(row=6,column=0,pady=10,padx=20,sticky="w")
 
-        txt_dob= Entry(Manage_Frame,textvariable=self.dob_var,font=("Arial",15,"bold"),bg="pink")
+        txt_dob= Entry(Manage_Frame,textvariable=self.dob_var,font=("Arial",15,"bold"),bg="lightblue")
         txt_dob.grid(row=6,column=1,pady=10,padx=20,sticky="w")
 
-        lbl_address= Label(Manage_Frame,text="Address",font=("Arial",15,"bold"),bg="pink")
+        lbl_address= Label(Manage_Frame,text="Address",font=("Arial",15,"bold"),bg="lightblue")
         lbl_address.grid(row=7,column=0,pady=10,padx=20,sticky="w")
         
-        self.txt_address = Text(Manage_Frame,width=31,height=4,font=("arial",10),bg="pink")
+        self.txt_address = Text(Manage_Frame,width=31,height=4,font=("arial",10),bg="lightblue")
         self.txt_address.grid(row=7,column=1,pady=10,padx=20,sticky="w")
 
     #=======Button_Frame========
-        btn_Frame= Frame(Manage_Frame,bd=4,relief=RIDGE,bg="pink")
+        btn_Frame= Frame(Manage_Frame,bd=4,relief=RIDGE,bg="lightblue")
         btn_Frame.place(x=10,y=500,width=420)
 
         Addbtn= Button(btn_Frame,text="Add",width=10,command=self.add_students).grid(row=0,column=0,padx=10,pady=10)
@@ -93,24 +94,24 @@ class Student:
         Clearbtn= Button(btn_Frame,text="Clear",width=10,command=self.clear).grid(row=0,column=3,padx=10,pady=10)
 
     #========Deatil_Frame=======
-        Detail_Frame = Frame(self.root,bd=4,relief=RIDGE,bg="pink")
+        Detail_Frame = Frame(self.root,bd=4,relief=RIDGE,bg="lightblue")
         Detail_Frame.place(x=500,y=100,width=800,height=590)
 
-        lbl_search= Label(Detail_Frame,text="Search By",font=("Arial",15,"bold"),bg="pink")
+        lbl_search= Label(Detail_Frame,text="Search By",font=("Arial",15,"bold"),bg="lightblue")
         lbl_search.grid(row=0,column=0,pady=10,padx=20,sticky="w")
 
         combo_search= ttk.Combobox(Detail_Frame,textvariable=self.search_by,width= 10,font=("Arial",15,"bold"),state="readonly")
         combo_search['values']=("Roll_no","Name","Contact")
         combo_search.grid(row=0,column=1,padx=20,pady=10)
 
-        txt_search= Entry(Detail_Frame,textvariable=self.search_txt,width=15,font=("Arial",15,"bold"),bg="pink")
+        txt_search= Entry(Detail_Frame,textvariable=self.search_txt,width=15,font=("Arial",15,"bold"),bg="lightblue")
         txt_search.grid(row=0,column=2,pady=10,padx=20,sticky="w")
 
         search_btn= Button(Detail_Frame,text="Search",width=10,pady=5,command=self.search_data).grid(row=0,column=3,padx=10,pady=10)
         showall_btn= Button(Detail_Frame,text="Show All",width=10,pady=5,command=self.fetch_data).grid(row=0,column=4,padx=10,pady=10)
     
     #========Table_Frame========
-        table_Frame=Frame(Detail_Frame,bd=4,relief=RIDGE,bg="pink")
+        table_Frame=Frame(Detail_Frame,bd=4,relief=RIDGE,bg="lightblue")
         table_Frame.place(x=15,y=70,width=760,height=450)
 
         scroll_x= Scrollbar(table_Frame,orient=HORIZONTAL)
